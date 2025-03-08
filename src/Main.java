a7c1d0a54279acc7798ddb66e54766b39b73b03b
MoskaikinSergey <153865570+MoskaikinSergey@users.noreply.github.com>
2025-03-08 16:59:46 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.Collection.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String name;
+        int age;
+
+        Scanner in = new Scanner(System.in);
+
+        List<User> users = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Введите имя пользователя " + (i + 1));
+            name = in.nextLine();
+            System.out.println("Введите возраст пользователя " + (i +1));
+            age = in.nextInt();
+            in.nextLine();
+
+            User temp = new User(name, age);
+            users.add(temp);
+        }
+        System.out.println("");
+
+        Comparator<User> com = new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                if (o1.getAge() > o2.getAge()) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+
+            }
+        };
+
+        Collections.sort(users, com);
+
+        for (int i = 0; i < users.size(); i++) {
+            System.out.println(users.get(i).toString());
+        }
+    }
+}
